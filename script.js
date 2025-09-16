86f221c0d46ce836061858ef2b0efe301b22ee96
Nale-kunal <nalekunal343@gmail.com>
2025-09-16 17:04:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 function stringChop(str, size) {
-  // your code here
-	size= Number(size);
-	if(str.length==0) return [];
-	let ans=[];
-	let arr="";
-	let count=0;
-	for(let i=0;i<str.length;i++){
-		if(count==size){
-			ans.push(arr);
-			count=0;
-			arr="";
-		}
-		
-		arr+=str[i];
-		count++;
-	}
-	if (arr.length > 0) ans.push(arr);
-	return ans;
+  if (str == null || str.length === 0) return [];     // avoid TypeError for null
+  size = Number(size);
+  if (!Number.isInteger(size) || size <= 0) return []; // validate size
+
+  let ans = [];
+  let arr = "";
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (count === size) {
+      ans.push(arr);
+      count = 0;
+      arr = "";
+    }
+    arr += str[i];
+    count++;
+  }
+  if (arr.length > 0) ans.push(arr);
+  return ans;
 }
 
+
 // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
